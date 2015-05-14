53c6fa5834095a0d2d8a24986d5145f1e8402d67
HasserJames <hasserveramendi@gmail.com>
2015-05-14 23:38:47 +0200

    Ontology for Sprint 4

---
+++ src/main/java/es/upm/fi/emse/hotelmania/Client.java
@@ -0,0 +1,26 @@
+package es.upm.fi.emse.hotelmania;
+
+
+import jade.content.*;
+import jade.util.leap.*;
+import jade.core.*;
+
+/**
+* Protege name: Client
+* @author ontology bean generator
+* @version 2015/05/14, 12:22:21
+*/
+public class Client implements Concept {
+
+   /**
+* Protege name: clientId
+   */
+   private String clientId;
+   public void setClientId(String value) { 
+    this.clientId=value;
+   }
+   public String getClientId() {
+     return this.clientId;
+   }
+
+}
---
+++ src/main/java/es/upm/fi/emse/hotelmania/RequestHotelBooking.java
@@ -0,0 +1,48 @@
+package es.upm.fi.emse.hotelmania;
+
+
+import jade.content.*;
+import jade.util.leap.*;
+import jade.core.*;
+
+/**
+* Protege name: RequestHotelBooking
+* @author ontology bean generator
+* @version 2015/05/14, 12:22:21
+*/
+public class RequestHotelBooking implements AgentAction {
+
+   /**
+* Protege name: numberOfDays
+   */
+   private int numberOfDays;
+   public void setNumberOfDays(int value) { 
+    this.numberOfDays=value;
+   }
+   public int getNumberOfDays() {
+     return this.numberOfDays;
+   }
+
+   /**
+* Protege name: RequestingClient
+   */
+   private Client requestingClient;
+   public void setRequestingClient(Client value) { 
+    this.requestingClient=value;
+   }
+   public Client getRequestingClient() {
+     return this.requestingClient;
+   }
+
+   /**
+* Protege name: startDate
+   */
+   private int startDate;
+   public void setStartDate(int value) { 
+    this.startDate=value;
+   }
+   public int getStartDate() {
+     return this.startDate;
+   }
+
+}
